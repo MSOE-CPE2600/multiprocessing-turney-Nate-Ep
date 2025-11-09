1f9f8d8becbb3f923de79cc4cc26c03d709a8727
Nathan Eppler <epplern@msoe.edu>
2025-11-09 17:17:03 -0600

    Added mandelmovie.c file

---
+++ mandelmovie.c
@@ -0,0 +1,17 @@
+/**
+* @file mandelmovie.c
+* @author Nathan Eppler <epplern@msoe.edu>
+* @date 9 November 2025
+* @brief Creates a short video zooming into a mandelbrot image set
+* 
+* Course: CPE2600
+* Section: 111
+* Due: 19 November 2025
+* 
+* To Compile: gcc -Wextra -o mandelmovie mandelmovie.c
+* To Run: ./mandelmovie <num processors>
+*/
+
+int main (int argc, char *argv[]) {
+
+}
