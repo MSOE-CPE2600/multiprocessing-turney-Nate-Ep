b9c26f1ff75d696cb60902fc653a494131de2b8f
Nathan Eppler <epplern@msoe.edu>
2025-11-11 10:42:19 -0600

    Implemented gen_images for one image

--- mandelmovie.c
+++ mandelmovie.c
@@ -12,8 +12,12 @@
 * To Run: ./mandelmovie <num processors>
 */
 
+#include <stdlib.h>
+#include <errno.h>
 #include <unistd.h>
 #include <stdio.h>
+
+void gen_images(int num_proc);
 
 int main (int argc, char *argv[]) {
 
@@ -22,7 +26,16 @@ int main (int argc, char *argv[]) {
     if (opt != -1) { //if all command line options have been parsed
         switch(opt) {
             case 'p':
-                printf("Option p selected\n");
+                printf("Option p selected\nArgument: %s\n", optarg);
+                char *endptr;
+                int num_proc = strtod(optarg, &endptr);
+
+                if (endptr == optarg || *endptr != '\0') {
+                    printf("Error: invalid number of processors '%s'\n", optarg);
+                    exit(EXIT_FAILURE);
+                }
+
+                gen_images(num_proc);
                 break;
             case ':':
                 printf("Error: No argument input\n");
@@ -34,5 +47,20 @@ int main (int argc, char *argv[]) {
                 printf("Error: no command found\n");
                 break;
         }
+    } else {
+        printf("Error: must use flag -p for proper execution\n");
     }
+}
+
+/**
+* @brief Generates 50 images using the specified number of processors
+* @param num_proc The number of processors to use to generate the 50 images (1-50)
+*/
+void gen_images(int num_proc) {
+    printf("Generating images\n");
+    char *args[] = {"./mandel", "-x", "-0.787", "-y", "-0.287", NULL}; //path of file to run, null terminated array of char args
+    errno = -1;
+    execvp(args[0], args);
+    perror("Error running execvp");
+    exit(EXIT_FAILURE);
 }
